ba7051c6e261e167ef0db53a410dfcfba23b0cf3
Tyler Burnett <11090043+TylerBurnett@users.noreply.github.com>
2020-06-17 19:49:16 +1000

    Added V8-compile-cache
    - Added new Node package V8-Compile-Cache to reduce load times and increase performance.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
+require("v8-compile-cache");
+const {
+  default: installExtension,
+  REACT_DEVELOPER_TOOLS,
+} = require("electron-devtools-installer");
 import { ComponentManager } from "./ComponentManager/ComponentManager";
-const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
-import { app, BrowserWindow } from "electron";
+import { app } from "electron";
 
 // Import debugging tools.
 app.whenReady().then(() => {
-    installExtension(REACT_DEVELOPER_TOOLS)
-        .then((name:any) => console.log(`Added Extension:  ${name}`))
-        .catch((err:any) => console.log('An error occurred: ', err));
+  installExtension(REACT_DEVELOPER_TOOLS)
+    .then((name: any) => console.log(`Added Extension:  ${name}`))
+    .catch((err: any) => console.log("An error occurred: ", err));
 
-    // Now lets start the app
-    var componentManager = new ComponentManager();
+  // Now lets start the app
+  var componentManager = new ComponentManager();
 });
-
