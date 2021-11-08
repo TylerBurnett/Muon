6ba95f72bc569236ce7634f4d601930e57667ba9
Tyler Burnett <11090043+TylerBurnett@users.noreply.github.com>
2021-11-08 17:46:01 +1000

    Culled all proposal polyfills
    - Massive Memory savings from removing all of the ECMA proposal polyfills

--- babel.config.js
+++ babel.config.js
@@ -26,6 +26,7 @@ module.exports = (api) => {
       [require('@babel/preset-react'), { development }],
     ],
     plugins: [
+      /*
       // Stage 0
       require('@babel/plugin-proposal-function-bind'),
 
@@ -41,12 +42,14 @@ module.exports = (api) => {
         require('@babel/plugin-proposal-nullish-coalescing-operator'),
         { loose: false },
       ],
+     
       [require('@babel/plugin-proposal-private-methods'), { loose: true }],
       [
         require('@babel/plugin-proposal-private-property-in-object'),
         { loose: true },
       ],
       require('@babel/plugin-proposal-do-expressions'),
+      
 
       // Stage 2
       [require('@babel/plugin-proposal-decorators'), { legacy: true }],
@@ -54,12 +57,14 @@ module.exports = (api) => {
       require('@babel/plugin-proposal-export-namespace-from'),
       require('@babel/plugin-proposal-numeric-separator'),
       require('@babel/plugin-proposal-throw-expressions'),
+    
 
       // Stage 3
       require('@babel/plugin-syntax-dynamic-import'),
       require('@babel/plugin-syntax-import-meta'),
-      [require('@babel/plugin-proposal-class-properties'), { loose: true }],
+      [require('@babel/plugin-proposal-class-properties')],
       require('@babel/plugin-proposal-json-strings'),
+      */
 
       ...(development ? developmentPlugins : productionPlugins),
     ],
